8b95bc3a0d344d09cc7085edf7f30a6210650571
rifki ardiansyah <rifkiardiansyah670@gmail.com>
2020-04-06 23:18:45 +0700

    add new post and book button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
 $(function () {
+
+    // Sidebar
     $('body').on('click', '.sidebar-toggle, .close-sidebar, .close-sidebar-toggle', function () {
         if ($('.sidebar').hasClass('active')) {
             $('.sidebar').removeClass('active');
@@ -6,6 +8,27 @@ $(function () {
         } else {
             $('.sidebar').addClass('active');
             $('.close-sidebar').fadeIn();
+        }
+    });
+
+    // share modal
+    $('body').on('click', '[data-toggle="modal-share"]', function () {
+        $('.share-modal').addClass('active');
+        $('.close-share-modal').fadeIn();
+    });
+    $('body').on('click', '.close-share-modal', function () {
+        $(this).fadeOut();
+        $('.share-modal').removeClass('active');
+    });
+
+    // Bottom bar
+    $('body').on('click', '.bottom-bar .menu.plus', function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $('.modal-menu-plus').removeClass('active');
+        } else {
+            $('.modal-menu-plus').addClass('active');
+            $(this).addClass('active');
         }
     });
 });
